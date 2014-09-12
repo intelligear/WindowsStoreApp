78a7130c8e49df5f0b3fd9353aef789588875361
Xin Peng <colorfulben@gmail.com>
2014-09-13 01:35:04 +0800

    Revert "Revert "Binding Enum values to multi selection control""
    This reverts commit ffd0b727b27f805719ed5c5f4104780078b2748c.

---
+++ EnumDropDownListBindingSample/EnumDropDownListBindingSample/Model/Fruit.cs
@@ -0,0 +1,17 @@
+﻿
+namespace EnumDropDownListBindingSample.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public enum Fruit
+    {
+        Apple,
+        Banana,
+        Cabernet
+    }
+}
---
+++ EnumDropDownListBindingSample/EnumDropDownListBindingSample/Model/OrderEntity.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace EnumDropDownListBindingSample.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public class OrderEntity : BindableBase
+    {
+        private string customerName;
+        private IEnumerable<Fruit> fruits = new List<Fruit>();
+        private Fruit favorite;
+
+
+        public string CustomerName
+        {
+            get { return this.customerName; }
+            set { SetProperty(ref this.customerName, value); }
+        }
+
+        public IEnumerable<Fruit> Fruits
+        {
+            get { return this.fruits; }
+            set { SetProperty(ref this.fruits, value); }
+        }
+        public Fruit Favorite
+        {
+            get { return this.favorite; }
+            set { SetProperty(ref this.favorite, value); }
+        }
+    }
+}
